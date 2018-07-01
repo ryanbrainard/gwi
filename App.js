8ff55d6ceb45cf62aef9c8bb117819d1fd39b3b6
Ryan Brainard <ryan@ryanbrainard.com>
2018-07-02 00:38:12 +0900

    Add prettier

--- App.js
+++ App.js
@@ -1,41 +1,38 @@
-import React from 'react';
-import {StyleSheet, View, FlatList, SafeAreaView} from 'react-native';
+import React from "react";
+import { StyleSheet, View, FlatList, SafeAreaView } from "react-native";
 import CharacterTile from "./CharacterTile";
 
 export default class App extends React.Component {
-
-    render() {
-        return (
-            <SafeAreaView style={styles.safeArea}>
-                <View style={styles.container}>
-                    <FlatList
-                        data={[
-                            {key: '기', color: 'red'},
-                            {key: '키', color: 'orange'},
-                            {key: '끼', color: 'yellow'},
-                            {key: '삼', color: 'green'},
-                            {key: '쌈', color: 'blue'},
-                        ]}
-                        renderItem={({item}) =>
-                            <CharacterTile
-                                char={item.key}
-                                color={item.color}
-                            />}
-                        numColumns={3}
-                    />
-                </View>
-            </SafeAreaView>
-        );
-    }
+  render() {
+    return (
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.container}>
+          <FlatList
+            data={[
+              { key: "기", color: "red" },
+              { key: "키", color: "orange" },
+              { key: "끼", color: "yellow" },
+              { key: "삼", color: "green" },
+              { key: "쌈", color: "blue" }
+            ]}
+            renderItem={({ item }) => (
+              <CharacterTile char={item.key} color={item.color} />
+            )}
+            numColumns={3}
+          />
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    safeArea: {
-        flex: 1,
-    },
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-        alignItems: 'center',
-    },
+  safeArea: {
+    flex: 1
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center"
+  }
 });
--- CharacterTile.js
+++ CharacterTile.js
@@ -1,49 +1,50 @@
-import React from 'react';
-import Expo from 'expo';
-import PropTypes from 'prop-types';
-import {Dimensions, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import assets from './assets'
+import React from "react";
+import Expo from "expo";
+import PropTypes from "prop-types";
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from "react-native";
+import assets from "./assets";
 
 export default class CharacterTile extends React.Component {
-    static propTypes = {
-        char: PropTypes.string,
-        color: PropTypes.string,
-    };
+  static propTypes = {
+    char: PropTypes.string,
+    color: PropTypes.string
+  };
 
-    render() {
-        const {char, color} = this.props;
+  render() {
+    const { char, color } = this.props;
 
-        return (
-            <View
-                style={[styles.container, {backgroundColor: color}]}>
-                <TouchableOpacity
-                    color={color}
-                    onPress={() => {
-                        Expo.Audio.Sound.create(
-                            assets.sounds[char],
-                            {shouldPlay: true}
-                        );
-                    }}>
-                    <Text style={styles.text}>
-                        {char}
-                    </Text>
-                </TouchableOpacity>
-            </View>
-        );
-    }
+    return (
+      <View style={[styles.container, { backgroundColor: color }]}>
+        <TouchableOpacity
+          color={color}
+          onPress={() => {
+            Expo.Audio.Sound.create(assets.sounds[char], { shouldPlay: true });
+          }}
+        >
+          <Text style={styles.text}>{char}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        width: Dimensions.get('window').width * 0.3,
-        height: Dimensions.get('window').width * 0.3,
-        borderWidth: 2,
-        borderColor: "white",
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    text: {
-        fontWeight: 'bold',
-        fontSize: 24,
-    }
+  container: {
+    width: Dimensions.get("window").width * 0.3,
+    height: Dimensions.get("window").width * 0.3,
+    borderWidth: 2,
+    borderColor: "white",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 24
+  }
 });
