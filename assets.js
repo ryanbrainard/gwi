d0562edfcf368fde7e220cf2aeb43bc4998418df
Ryan Brainard <ryan@ryanbrainard.com>
2018-07-02 00:12:15 +0900

    Add more sounds

--- assets.js
+++ assets.js
@@ -1,6 +1,12 @@
 export default {
     sounds: {
-        '키': require('./assets/sounds/ki.mp3'),
+        '기': require('./assets/sounds/gi.mp3'),
         '끼': require('./assets/sounds/ggi.mp3'),
+        '도': require('./assets/sounds/do.mp3'),
+        '벼': require('./assets/sounds/byuh.mp3'),
+        '삼': require('./assets/sounds/sam.mp3'),
+        '쌈': require('./assets/sounds/ssam.mp3'),
+        '절': require('./assets/sounds/jeol.mp3'),
+        '키': require('./assets/sounds/ki.mp3'),
     }
 };
