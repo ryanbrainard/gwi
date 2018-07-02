470d9c938121e0d459d74ba71396a105bfd5264a
Ryan Brainard <ryan@ryanbrainard.com>
2018-07-02 23:01:42 +0900

    Add dynamic colors

--- components/CharacterGrid.js
+++ components/CharacterGrid.js
@@ -16,14 +16,26 @@ export default class CharacterGrid extends React.Component {
         <FlatList
           data={data}
           renderItem={({ item }) => (
-            <CharacterTile char={item.key} color={item.color} />
+            <CharacterTile char={item.key} color={this.nextColor()} />
           )}
           numColumns={3} // TODO: customizable and change tiles
         />
       </View>
     );
   }
+
+  colorCursor = 0;
+
+  nextColor() {
+    if (this.colorCursor >= colors.length) {
+      this.colorCursor = 0;
+    }
+
+    return colors[this.colorCursor++];
+  }
 }
+
+const colors = ["#375E97", "#FB6542", "#FFBB00", "#3F681C"];
 
 const styles = StyleSheet.create({
   container: {
