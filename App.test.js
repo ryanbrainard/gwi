3e1ba240ac5f8d182689b47dab58e5c95b52a82d
Ryan Brainard <ryan@ryanbrainard.com>
2018-07-06 14:39:16 +0900

    Shitty workaround for Jest problem

--- App.test.js
+++ App.test.js
@@ -3,7 +3,8 @@ import App from "./App";
 
 import renderer from "react-test-renderer";
 
-it("renders without crashing", () => {
+it("renders without crashing", done => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
+  setImmediate(done); // shitty workaround to Jest problem (https://github.com/facebook/jest/issues/4710); TODO: work on a real fix.
 });
