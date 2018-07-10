6cc86c6a70a2687c208c69b23724287577ae9e0b
Ryan Brainard <ryan@ryanbrainard.com>
2018-07-10 21:11:37 +0900

    Add chars to grid test

--- components/CharacterGrid.test.js
+++ components/CharacterGrid.test.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Character from "../models/Character";
 import CharacterGrid from "./CharacterGrid";
 import renderer from "react-test-renderer";
 
 test("renders correctly", () => {
-  const tree = renderer.create(<CharacterGrid />).toJSON();
+  const tree = renderer
+    .create(<CharacterGrid chars={[new Character("A"), new Character("B")]} />)
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
--- models/CharacterSet.test.js
+++ models/CharacterSet.test.js
@@ -2,7 +2,6 @@ import CharacterSet from "./CharacterSet";
 
 it("#all", () => {
   const all = CharacterSet.all();
-  console.log(">>>>>", all);
   expect(all.length).toBe(2);
   expect(all[0].name).toBe("ㄱㄲㅋ");
   expect(all[0].characters.length).toBe(3);
