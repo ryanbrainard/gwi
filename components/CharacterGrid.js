1de2508044c3a8d85c95c621d1565049e6f851d9
Ryan Brainard <ryan@ryanbrainard.com>
2018-07-11 00:00:45 +0900

    Move colors to ColorIterator

--- components/CharacterGrid.js
+++ components/CharacterGrid.js
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import React from "react";
 import { FlatList, StyleSheet, View } from "react-native";
 import Character from "../models/Character";
+import ColorIterator from "../models/ColorIterator";
 import CharacterTile from "./CharacterTile";
 
 export default class CharacterGrid extends React.Component {
@@ -11,32 +12,21 @@ export default class CharacterGrid extends React.Component {
 
   render() {
     const { chars } = this.props;
+    const colors = ColorIterator.default();
 
     return (
       <View style={styles.container}>
         <FlatList
           data={chars}
           renderItem={({ item, index }) => (
-            <CharacterTile char={item} color={this.nextColor()} />
+            <CharacterTile char={item} color={colors.next()} />
           )}
           numColumns={3} // TODO: customizable and change tiles
         />
       </View>
     );
   }
-
-  colorCursor = 0;
-
-  nextColor() {
-    if (this.colorCursor >= colors.length) {
-      this.colorCursor = 0;
-    }
-
-    return colors[this.colorCursor++];
-  }
 }
-
-const colors = ["#375E97", "#FB6542", "#FFBB00", "#3F681C"];
 
 const styles = StyleSheet.create({
   container: {
