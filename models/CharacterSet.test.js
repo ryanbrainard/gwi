a895e4e669cb049310a5d80763deb9ec832e6466
Ryan Brainard <ryan@ryanbrainard.com>
2018-08-12 23:42:41 +0900

    Fix tests

--- models/CharacterSet.test.js
+++ models/CharacterSet.test.js
@@ -2,8 +2,8 @@ import CharacterSet from "./CharacterSet";
 
 it("#list", () => {
   const all = CharacterSet.list();
-  expect(all.length).toBe(2);
+  expect(all.length).toBe(5);
   expect(all[0].name).toBe("ㄱㄲㅋ");
-  expect(all[0].characters.length).toBe(3);
-  expect(all[0].characters[0].name).toBe("기");
+  expect(all[0].characters.length).toBe(12);
+  expect(all[0].characters[0].name).toBe("겁");
 });
