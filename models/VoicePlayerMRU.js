2aeab9214a2e5053dbe749c1462f18c98a235001
Ryan Brainard <ryan@ryanbrainard.com>
2018-12-28 15:49:36 +0900

    Rearrange MRU to avoid loading race.

--- models/VoicePlayerMRU.js
+++ models/VoicePlayerMRU.js
@@ -22,30 +22,27 @@ export default class VoicePlayerMRU {
       return;
     }
 
-    if (this.playback._loading) {
-      // isn't in status api...
-      console.log("fn=VoicePlayerMRU.load at=noop-loading");
-      return;
-    }
-
-    await this.playback.loadAsync(this.voices.jane); // TODO: settings
+    console.log("fn=VoicePlayerMRU.load at=mru.add");
     VoicePlayerMRU.recentPlaybacks.unshift(this.playback);
 
-    console.log("fn=VoicePlayerMRU.load at=mru.start");
+    console.log("fn=VoicePlayerMRU.load at=mru.gc");
     while (
       VoicePlayerMRU.recentPlaybacks.length > VoicePlayerMRU.maxPlaybacks
     ) {
-      console.log("fn=VoicePlayerMRU.load at=mru.pop");
+      console.log("fn=VoicePlayerMRU.load at=mru.gc.pop");
       const pb = VoicePlayerMRU.recentPlaybacks.pop();
       if ((await pb.getStatusAsync()).isLoaded) {
         console.log("fn=VoicePlayerMRU.load at=mru.unload");
-        await pb.unloadAsync();
+        pb.unloadAsync();
       }
     }
     console.log(
       "fn=VoicePlayerMRU.load at=mru.count",
       VoicePlayerMRU.recentPlaybacks.length
     );
+
+    console.log("fn=VoicePlayerMRU.load at=mru.load");
+    await this.playback.loadAsync(this.voices.jane); // TODO: settings
 
     console.log("fn=VoicePlayerMRU.load at=finish");
   }
