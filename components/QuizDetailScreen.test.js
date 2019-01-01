5390eec81d4f4813c2ebda91cd6f9765179db5b1
Ryan Brainard <ryan@ryanbrainard.com>
2019-01-01 11:13:51 +0900

    Add chars to quiz detail screen test

--- components/QuizDetailScreen.test.js
+++ components/QuizDetailScreen.test.js
@@ -1,12 +1,18 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import Character from "../models/Character";
 import QuizDetailScreen from "./QuizDetailScreen";
 
 test("renders correctly", () => {
   jest.mock("Animated");
 
+  const group = ["A", "B"];
+  const characters = group.map(char =>
+    Character.register(new Character(char, group))
+  );
+
   const navigation = {
-    getParam: jest.fn(() => ({ characters: [] }))
+    getParam: jest.fn(() => ({ characters }))
   };
 
   const tree = renderer
